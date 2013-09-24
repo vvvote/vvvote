d3b97bd761cd0b898ab1818df9d8bc147175f2c1
pfefffer <vvvote@robert-arnold.de>
2013-09-24 22:29:26 +0200

    bug fix: require to not used library removed changed default page to "an Abstimmung teilnehmen"

--- backend/modules-tally/publishonly/tally.php
+++ backend/modules-tally/publishonly/tally.php
@@ -7,7 +7,6 @@
  */
 
 require_once __DIR__ . '/../../crypt.php';
-require_once __DIR__ . '/../../Crypt/AES.php';
 require_once __DIR__ . '/../../exception.php';
 require_once __DIR__ . '/dbPublishOnlyTally.php';
 
