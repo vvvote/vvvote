ec5f5add7301df349a886d4422f59a9a6eaae3c5
pfefffer <vvvote@robert-arnold.de>
2014-02-05 20:27:10 +0100

    bug fix in connection checking

--- backend/connectioncheck.php
+++ backend/connectioncheck.php
@@ -9,7 +9,10 @@ if (isset($_GET['connectioncheck'])) {
 	print ("<!DOCTYPE html>\n<html>\n<head>\n<meta charset=\"ISO-8859-1\">");
 	print ('<title>VVVote: Verbindungstest erfolreich</title>');
 	print ('</head><body>');
-	print ('<big><u>Verbindungstest erfolgreich.</u></big> <ul><li>Schlie&szlig;en Sie jetzt dieses Fenster und </li><li>klicken Sie in dem urspr&uuml;nglichen Fenster innerhalb des roten Balkens auf den Knopf &quot;erneut versuchen&quot;</li><ul>');
+	print ('<big><u>Verbindungstest erfolgreich.</u></big>'); 
+    print ('<ul><li>Schlie&szlig;en Sie jetzt dieses Fenster und </li>');
+    print ('    <li>klicken Sie in dem urspr&uuml;nglichen Fenster innerhalb des roten Balkens auf den Knopf &quot;erneut versuchen&quot;</li></ul>');
+//    print ('<button onlick="window.close();">Fenster schlie&szlig;en</button>');
 	print ('</body>');
 	exit(0);
 }
