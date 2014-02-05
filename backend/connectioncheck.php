c694b9a774f07393802ea64bcf0349ef17d26912
pfefffer <example@example.com>
2014-02-05 19:17:37 +0100

    bug fixed: add forgotten file in one of the last commits

---
+++ backend/connectioncheck.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * This is for connection checking from the client
+ * It should be included by any php-file that is called from the client
+ * as first required_once
+ */
+
+if (isset($_GET['connectioncheck'])) {
+	print ("<!DOCTYPE html>\n<html>\n<head>\n<meta charset=\"ISO-8859-1\">");
+	print ('<title>VVVote: Verbindungstest erfolreich</title>');
+	print ('</head><body>');
+	print ('<big><u>Verbindungstest erfolgreich.</u></big> <ul><li>Schlie&szlig;en Sie jetzt dieses Fenster und </li><li>klicken Sie in dem urspr&uuml;nglichen Fenster innerhalb des roten Balkens auf den Knopf &quot;erneut versuchen&quot;</li><ul>');
+	print ('</body>');
+	exit(0);
+}
+
+?>
