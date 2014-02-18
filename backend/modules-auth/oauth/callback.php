d7e9f37e389dfa300a3632c7b49d3bfb80a9204d
pfefffer <example@example.com>
2014-02-18 10:08:05 +0100

    work on OAuth 2.0 / BEO

--- backend/modules-auth/oauth/callback.php
+++ backend/modules-auth/oauth/callback.php
@@ -15,6 +15,8 @@ require 'client.php';
 require 'GrantType/IGrantType.php';
 require 'GrantType/AuthorizationCode.php';
 
+// TODO remove this and put it in the conf-this-server
+$oauthConfig['is_in_voter_list_endp'] = 'https://beoauth.piratenpartei-bayern.de/api/self/listmember/';
 
 // $client = new OAuth2\Client(CLIENT_ID, CLIENT_SECRET);
 if (!isset($_GET['code']))
@@ -33,20 +35,20 @@ else
 	//	print_r($oauthdata);
 	$curOAuth2Config = $oauthConfig[$state[0]];
 	/*
-	// unescpe '..' to '.'
+	 // unescpe '..' to '.'
 	$i = 1; $next = true;
 	while ($next && $i < count($state)) {
-		$electionId = $oauthConfig[$state[$i]];
-		if (strlen($state[$i]) == 0) {
-			$electionId = $electionId . '.';
-			$next = true;
-		} else {
-			$next = false;
-		}
-		$i++;
+	$electionId = $oauthConfig[$state[$i]];
+	if (strlen($state[$i]) == 0) {
+	$electionId = $electionId . '.';
+	$next = true;
+	} else {
+	$next = false;
+	}
+	$i++;
 	}
 	*/
-	
+
 	$electionhash = $state[1];
 	$tmpsecret = $state[2];
 	//	print "<br><br>\ncurConfig: ";
@@ -62,7 +64,12 @@ else
 	//	print "<br><br>\info: ";
 	print_r($tokeninfos);
 	$client->setAccessToken($tokeninfos);
-
+	
+	$fetcher = new FetchFromOAuth2Server($state[0], $tokeninfos);
+	print '<br>username from fetch: ' . $fetcher->fetchUsername();
+	print '<br>auid from fetch: ' . $fetcher->fetchAuid();
+	print '<br>isInVoterlist from fetch: ' . $fetcher->isInVoterList('d94b915b-db13-4264-890c-0780692e4998');
+	
 	$membership = $client->fetch($curOAuth2Config['get_membership_endp'], Array(), Client::HTTP_METHOD_POST);
 	print "<br><br>\nresponse 2: ";
 	print_r($membership);
@@ -71,12 +78,14 @@ else
 	$userprofile = $client->fetch('https://beoauth.piratenpartei-bayern.de/api/self/profile/', $params, Client::HTTP_METHOD_POST);
 	print "<br><br>\nresponse 3: ";
 	print_r($userprofile);
-
-	$mayvote = $client->fetch($curOAuth2Config['may_vote_endp'] . 'd94b915b-db13-4264-890c-0780692e4998' .'/', Array(), Client::HTTP_METHOD_POST);
+	$listId = 'd94b915b-db13-4264-890c-0780692e4998';
+	$mayvote = $client->fetch($curOAuth2Config['is_in_voter_list_endp'] . $listId .'/', Array(), Client::HTTP_METHOD_POST);
 	print "<br><br>\may vote: ";
 	print_r($mayvote);
+	$mayvoteBoolean = ($mayvote['result']['list'] === $listId && $mayvote['result'] == 1);
 	$now =  new DateTime('now');
-	
+
+
 	print ("<!DOCTYPE html>\n<html>\n<head>\n<meta charset=\"ISO-8859-1\">");
 	print ('<title>VVVote: Login erfolgreich</title>');
 	print ('</head><body>');
@@ -84,7 +93,13 @@ else
 	print ('<ul><li>Schlie&szlig;en Sie jetzt dieses Fenster und </li>');
 	print ('    <li>klicken Sie in dem urspr&uuml;nglichen Fenster auf den Knopf zum Login für Abstimmungsserver 2</li></ul>');
 	print ('<button onClick="window.close();">Fenster schlie&szlig;en</button>');
-	
+
+	$ret = array(
+			'usernanme' => $userprofile['result']['username'],
+			'auid'      => $membership['result']['auid'],
+			'mayvote'   => $mayvoteBoolean
+	);
+
 	print "<h1>Ergebnisse</h1>";
 	print '<br>auid: ' . $membership['result']['auid'];
 	print '<br>Access Token: ' . print_r($tokeninfos, true);
@@ -96,6 +111,39 @@ else
 	print "<br>jetzt: " . $now->format(DateTime::ATOM);
 	print ('</body>');
 	// var_dump($response, $response['result']);
+}
+
+class FetchFromOAuth2Server {
+
+	function __construct($serverId, $authInfos) {
+		//$this->serverId = $serverId;
+		//$this->authInfos = $authInfos;
+		$this->curOAuth2Config =  $oauthConfig[$serverId];
+		$this->client = new OAuth2\Client($this->curOAuth2Config['client_id'], $this->curOAuth2Config['client_secret']);
+		$client->setAccessToken($authInfos);
+		// $this->params = array('redirect_uri' => $curOAuth2Config['redirect_uri']); // $params = array('code' => $_GET['code'], 'redirect_uri' => $curOAuth2Config['redirect_uri']);
+	}
+
+	function fetchUsername() {
+		$userprofile = $this->fetch($this->curOAuth2Config['get_profile_endp']);
+		return $userprofile['result']['username'];
+	}
+
+	function fetchAuid() {
+		$membership = $this->fetch($this->curOAuth2Config['get_membership_endp']);
+		return $membership['result']['auid'];
+	}
+
+	function isInVoterList($listId) {
+		$inVoterlist = $this->fetch($this->$curOAuth2Config['is_in_voter_list_endp'] . $listId .'/');
+		$inVoterlistBoolean = ($inVoterlist['result']['list'] === $listId && $inVoterlist['result'] == 1);
+		return $$inVoterlistBoolean;
+	}
+
+
+	function fetch($endpoint) {
+		return $this->client->fetch($endpoint, Array(), Client::HTTP_METHOD_POST);
+	}
 }
 
 
