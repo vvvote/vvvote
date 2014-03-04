6f9dd080ea2b1b257e894fc2c7a35935c8ecbeec
pfefffer <vvvote@robert-arnold.de>
2014-03-04 13:41:55 +0100

    BEO: better user guidance

--- webclient/modules-auth/oauth2/module.js
+++ webclient/modules-auth/oauth2/module.js
@@ -24,7 +24,7 @@ OAuth2.random = [];
 
 
 function setSubStep(ss) {
-	var numSteps = 4;
+	var numSteps = 3;
 	for (var i=1; i<=numSteps; i++) { 
 		var el  = document.getElementById("substep"+i);
 		var el2 = document.getElementById("ss"+i);
@@ -58,21 +58,21 @@ OAuth2.getMainContent = function(conf) {
 	var step = 1;
 	var mc = 
 		'<ol class="substep-progress">' +
-		'<li class="active-step" id="ss1">' +
+/*		'<li class="active-step" id="ss1">' +
         '	<span class="step-name"><span class="substeps">A:</span> Einloggen</span>' +
         '</li>' +
-        '<li class="todo-step"   id="ss2">' +
-        '	<span class="step-name"><span class="substeps">B:</span> Server 1 Zugriff erlauben</span>'+
+*/      '<li class="active-step"   id="ss1">' +
+        '	<span class="step-name"><span class="substeps">A:</span> Abstimmserver autorisieren</span>'+
+        '</li>'+
+        '<li class="todo-step"   id="ss2">'+
+        '<span class="step-name"><span class="substeps">B:</span> Kontrollserver autorisieren</span>'+
         '</li>'+
         '<li class="todo-step"   id="ss3">'+
-        '<span class="step-name"><span class="substeps">C:</span> Server 2 Zugriff erlauben</span>'+
-        '</li>'+
-        '<li class="todo-step"   id="ss4">'+
-        '	<span class="step-name"><span class="substeps">D:</span> Wahlschein erstellen</span>'+
+        '	<span class="step-name"><span class="substeps">C:</span> Wahlschein erstellen</span>'+
         '</li>'+
         '</ol>'+
         '<br><br>' +
-		'<div id="substep1">	<label for="loginOauth2Txt"><span class="substeps">Schritt ' + String.fromCharCode('A'.charCodeAt(0) + step -1) +':</span> Einloggen</label> ' +
+/*		'<div id="substep1">	<label for="loginOauth2Txt"><span class="substeps">Schritt ' + String.fromCharCode('A'.charCodeAt(0) + step -1) +':</span> Einloggen</label> ' +
 		'<span id="loginOauth2Txt">Sie m&uuml;ssen sich beim Basisentscheid-Server einloggen und angemeldet bleiben. </span>' +
 		'						<label for="loginOAuth2"><span class="substeps"> </label> ' +
 		'		  				     <a id="loginOAuth2" href="' + ClientConfig.oAuth2Config[serverId].loginUri + '" target="_blank" onclick="OAuth2.showDoneButton(\'loginOauth2Txt2\');">&Uuml;ber &gt;' + ClientConfig.oAuth2Config[serverId].serverDesc + '&lt; einloggen</a><br>' +
@@ -80,14 +80,16 @@ OAuth2.getMainContent = function(conf) {
 		'						<label for="loginOauth2Txt2"> </label> ' +
 		'<span id="loginOauth2Txt2" style="display:none;"><button onclick="setSubStep(2);">Ich habe mich erfolreich angemeldet</button></span>'+
 		'</div>'+
+*/
 //	'<span id="loginOauth2Txt2">Wichtig: Erst einloggen, danach folgende Schritte ausf&uuml;hren.'+
 //	'<span id="loginOauth2Txt2">Erst nachdem Sie sich erfolgreich eingeloggt haben, fahren Sie mit den folgenden Schritten fort.</span><br>' + // klicken Sie auf die folgenden ' + Object.keys(ClientConfig.oAuth2Config[serverId].clientId).length + ' Links.'
 
 	// ' Durch den Klick auf die Links wird den Abstimmungsserver erlaubt beim Basisentscheid-Server ' +
 	// 'Ihre Wahlberechtigung abzurufen.' +
 		'<br>';
-	step++;
+//	step++;
 	
+	var slist = getPermissionServerList();
 	for ( var permissionServerId in ClientConfig.oAuth2Config[serverId].clientId) {
 		var clientId = ClientConfig.oAuth2Config[conf.authConfig.serverId].clientId[permissionServerId];
 		OAuth2.random[clientId] = bigInt2str(randBigInt(200,0), 62);
@@ -97,14 +99,17 @@ OAuth2.getMainContent = function(conf) {
 		'&redirect_uri=' + ClientConfig.oAuth2Config[serverId].redirectUri[permissionServerId] + 
 		'&response_type=code' +
 		'&client_id=' + clientId;
+		var permServerNr =  ArrayIndexOf(slist, 'name', permissionServerId);
+		var style = '"display:none;"';
+		if (step == 1) style = '""';
 		mc = mc + 
-		'<div id="substep' + step +'" style="display:none;">' +
-		'						<label for="login'+step+'"><span class="substeps">Schritt ' + String.fromCharCode('A'.charCodeAt(0) + step -1) +':</span> Für ' + permissionServerId +': </label> ' +
+		'<div id="substep' + step +'" style=' + style + '>' +
+		'						<label for="login'+step+'"><span class="substeps">Schritt ' + String.fromCharCode('A'.charCodeAt(0) + step -1) +':</span> Für ' + slist[permServerNr].desc +': </label> ' +
 	//	'		  				     <a id="login" href="javascript:window.open(\'' + oauthAutorize + '\', \'_blank\');">Zugriff auf &gt;' + ClientConfig.oAuth2Config[serverId].serverDesc + '&lt; erlauben</a><br>';
-		'		  				     <a id="login'+step+'" href="' + oauthAutorize + '" target="_blank" onclick="OAuth2.showDoneButton(\'loginOauth2Txt2s'+step+'\');">Zugriff auf &gt;' + ClientConfig.oAuth2Config[serverId].serverDesc + '&lt; erlauben</a><br>'+
+		'		  				     <a id="login'+step+'" href="' + oauthAutorize + '" target="_blank" onclick="OAuth2.showDoneButton(\'loginOauth2Txt2s'+step+'\');">' + slist[permServerNr].desc + ' autorisieren</a><br>'+
 //		'		  				     <a id="login" href="' + oauthAutorize + '" target="_blank">&Uuml;ber &gt;' + ClientConfig.oAuth2Config[serverId].serverDesc + '&lt; einloggen</a><br>';
 		'						<label for="loginOauth2Txt2s'+step+'"> </label> ' +
-		'<span id="loginOauth2Txt2s'+step+'" style="display:none;"><button onclick="setSubStep(' +(step+1) +')">Zugriff erfolgreich erlaubt</button></span>'+
+		'<span id="loginOauth2Txt2s'+step+'" style="display:none"><button onclick="setSubStep(' +(step+1) +')">Zugriff erfolgreich erlaubt</button></span>'+
 		'<br>' +
 		'</div>';
 		step++;
@@ -112,7 +117,7 @@ OAuth2.getMainContent = function(conf) {
 	
 	mc = mc +
 	'<div id="substep' + step +'" style="display:none;">' +
-	'						<label for="username"><span class="substeps">Schritt ' + String.fromCharCode('A'.charCodeAt(0) + step -1) +':</span> Username beim Basisentscheid-Server</label> ' +
+	'						<label for="username"><span class="substeps">Schritt ' + String.fromCharCode('A'.charCodeAt(0) + step -1) +':</span> Username erneut eingeben</label> ' +
 	'						     <input name="username" id="username" value="" type="text"></td>' + 
     '                       <br>' +
 	'						<label for="displayname" style="display:none">Mich &ouml;ffentlich anzeigen als</label> ' +
