bd8977c7ccd55801125b48f1452bd3a40012b464
pfefffer <vvvote@robert-arnold.de>
2014-08-25 22:36:41 +0200

    introduce object-oriented config-style (forgot to commit this file in some previous commit)

--- webclient/config/config-example.js
+++ webclient/config/config-example.js
@@ -4,34 +4,25 @@
  */
 
 var server1url = 'http://www.webhod.ra/vvvote2/backend/';
-var server2url = 'http://www2.webhod.ra/vvvote2/backend/';
+var server2url = 'http://127.0.0.1/vvvote2/backend/';
 
-newElectionUrl    = new Array(server1url +'newelection.php',
-		                      server2url +'newelection.php');
-electionConfigUrl = server1url + 'getelectionconfig.php';
-tallyUrl          = server1url + 'tallyvote.php'; //?XDEBUG_SESSION_START=ECLIPSE_DBGP&KEY=13727034088813';
+var server1urlParts = URI.getParts(server1url);
 
-// OAuth 2.0 configuration
+function ClientConfig() {
+	 
+}
 
-oAuth2ConfigBEOBayern = {
-		serverId: 'BEOBayern', // This is used in the backend to identify the oauth server
-		serverDesc: 'Basisentscheid Online der Piraten (Bayrischer Testserver)',
-		authorizeUri: 'https://beoauth.piratenpartei-bayern.de/oauth2/authorize/?', // must end with ?
-		scope: 'member+profile',
-		redirectUri: {
-			0: 'https://abstimmung.piratenpartei-nrw.de/backend/modules-auth/oauth/callback.php',
-			1: 'https://84.246.124.167/backend/modules-auth/oauth/callback.php'
-		}, 
-		clientId: {
-			0: 'vvvote',
-			1: 'vvvote2'
-		}
-};
 
-oAuth2Config = {
-		'BEOBayern': oAuth2ConfigBEOBayern
-		};
+ClientConfig.newElectionUrl    = new Array(	server1url +'newelection.php',
+											server2url +'newelection.php');
+ClientConfig.electionConfigUrl = server1url + 'getelectionconfig.php';
+ClientConfig.storeVoteUrl      = 'http://' + server1urlParts.host + server1urlParts.pathname + 'storevote.php'; //do not use https here to enable the anonymizer-server to strip the browser-fingerprint - this is not necessary if all voters would use the tor browser bundle
+ClientConfig.getResultUrl      = server1url + 'getresult.php'; //?XDEBUG_SESSION_START=ECLIPSE_DBGP&KEY=13727034088813';
 
+
+ClientConfig.serverList = getPermissionServerList();
+ClientConfig.anonymizerUrl = ''; // http://anonymouse.org/cgi-bin/anon-www_de.cgi/'; // used to change the ip and to strip browser infos / with trailing slash
+ClientConfig.voteClientUrl = server1url + 'getclient.php';
 
 function getPermissionServerList() {
 	// load config
@@ -43,6 +34,7 @@ function getPermissionServerList() {
 	var key0    = new Object();
 	var key1    = new Object();
 	server0.name = 'PermissionServer1';
+	server0.desc = 'Abstimmserver';
 	server0.url  = server1url + 'getpermission.php'; // 'getpermission.php?XDEBUG_SESSION_START=ECLIPSE_DBGP&KEY=13727034088813';
 	key0.exp     = str2bigInt('65537', 10);  
 	key0.exppriv = str2bigInt('1210848652924603682067059225216507591721623093360649636835216974832908320027478419932929', 10); //@TODO remove this bvefore release, only needed for debugging
@@ -52,6 +44,7 @@ function getPermissionServerList() {
 	slist[0] = server0;
 	
 	server1.name = 'PermissionServer2';
+	server1.desc = 'Kontrollserver';
 	server1.url = server2url + 'getpermission.php';
 	key1.exp     = str2bigInt('65537', 10);  
 	key1.exppriv = str2bigInt('1210848652924603682067059225216507591721623093360649636835216974832908320027478419932929', 10); //@TODO remove this bvefore release, only needed for debugging
@@ -63,3 +56,36 @@ function getPermissionServerList() {
 	
     return slist;	
 };
+
+
+// configs for OAuth 2.0 Servers
+var redirectUriTMP = [];
+redirectUriTMP[ClientConfig.serverList[0].name] = 'http%3a//www.webhod.ra/vvvote2/backend/modules-auth/oauth/callback.php';
+redirectUriTMP[ClientConfig.serverList[1].name] = 'http%3a//www2.webhod.ra/vvvote2/backend/modules-auth/oauth/callback.php'; //https://84.246.124.167/backend//modules-auth/oauth/callback.php
+
+var clientIdTMP = [];
+clientIdTMP[ClientConfig.serverList[0].name] = 'vvvote';
+clientIdTMP[ClientConfig.serverList[1].name] = 'vvvote2';
+
+
+var oAuth2ConfigBEOBayern = {
+		serverId: 		'BEOBayern', // This is used in the backend to identify the oauth server
+		serverDesc: 	'Basisentscheid Online der Piraten (Bayerischer Testserver)',
+		authorizeUri: 	'https://beoauth.piratenpartei-bayern.de/oauth2/authorize/?', // must end with ?
+		loginUri:		'https://beoauth.piratenpartei-bayern.de/',
+		scope: 			'member+profile',
+		redirectUri: 	redirectUriTMP,
+		clientId: 		clientIdTMP
+};
+
+ClientConfig.oAuth2Config = {
+			'BEOBayern': oAuth2ConfigBEOBayern
+			};
+
+ClientConfig.getServerInfoByName = function (servername) {
+	var slist = ClientConfig.serverList;
+	for (var s=0; s<slist.length; s++) {
+		if (slist[s].name == servername) return slist[s]; 
+	}
+};
+
