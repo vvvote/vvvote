eccd951d6897ec6e9dbb07144bc78dfa1b48c69f
pfefffer <vvvote@robert-arnold.de>
2015-03-12 09:58:04 +0100

    bug fixed: backend: added missing file

---
+++ backend/blinder.php
@@ -0,0 +1,16 @@
+<?php
+
+class Blinder {
+    
+	/**
+	 * here a new private key could be generated and the public key returned
+	 * @param unknown $electionId
+	 * @return multitype:
+	 */
+	function handleNewElectionReq($electionId) {
+		return array();
+	}
+	
+}
+
+?>
