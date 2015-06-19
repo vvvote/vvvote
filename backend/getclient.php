12e7e5548c4214045c43d6c7ff82bc4668991d7f
pfefffer <vvvote@robert-arnold.de>
2015-06-20 00:57:24 +0200

    fix for badly configured servers: make sure that charset utf-8 ist set in http header

--- backend/getclient.php
+++ backend/getclient.php
@@ -13,7 +13,7 @@ header("Expires: Sat, 01 Jan 2005 00:00:00 GMT");
 header("Last-Modified: ".gmdate( "D, d M Y H:i:s")."GMT");
 */
 header("Cache-Control: no-cache, must-revalidate");
-
+header('Content-type: text/html; charset=utf-8');
 
 $pathToClient = '../webclient/';
 
