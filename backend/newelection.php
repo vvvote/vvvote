31d2d6c14212479ffe00dd26e88b4821cc1f2b72
pfefffer <vvvote@robert-arnold.de>
2016-01-01 11:44:19 +0100

    add LICENSE file

--- backend/newelection.php
+++ backend/newelection.php
@@ -1,7 +1,27 @@
 <?php
 
+/*
+    VVVote: An Anonymity and Traceability Providing Online Voting System
+    Copyright (C) 2016 Robert Arnold, prog@robert-arnold.de
 
- require_once 'connectioncheck.php';  // answers if &connectioncheck is part of the URL and exists
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+ */
+
+
+
+require_once 'connectioncheck.php';  // answers if &connectioncheck is part of the URL and exists
 
 require_once __DIR__ . '/config/conf-allservers.php';
 require_once 'config/conf-thisserver.php';
