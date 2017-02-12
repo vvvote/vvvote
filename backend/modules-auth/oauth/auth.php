vvvote/vvvote

commit 74afa51bac0b94bfdf6fbf99d95d42029df0a5a9

* make easier using ports different from standard ports * backend: OAuth2: corrected and added an error message * frontend: Oauth2: added an error message if a OAuth-Server was asked for in the election config which is not configured in the OAuth-config

backend/modules-auth/oauth/auth.php
@@ -173,12 +173,13 @@ class OAuth2 extends Auth {
 	function handleNewElectionReq($electionId, $req) {
 		$authconfig = parent::handleNewElectionReq($electionId, $req);
 		
-		if ( (! isset($req["serverId"]     )) || (! is_string($req['serverId']     )) ) WrongRequestException::throwException(12001, 'Missing /listId/ in election config'       , "request received: \n" . print_r($req, true));
+		if ( (! isset($req["serverId"]     )) || (! is_string($req['serverId']     )) ) WrongRequestException::throwException(12006, 'Missing /serverId/ in election config'     , "request received: \n" . print_r($req, true));
 		if ( (! isset($req["listId"]       )) || (! is_string($req['listId']       )) ) WrongRequestException::throwException(12001, 'Missing /listId/ in election config'       , "request received: \n" . print_r($req, true));
 		if ( (! isset($req["nested_groups"])) || (! is_array( $req['nested_groups'])) ) WrongRequestException::throwException(12002, 'Missing /nested_groups/ in election config', "request received: \n" . print_r($req, true));
 		if ( (! isset($req["verified"]     )) || (! is_bool(  $req['verified']     )) ) WrongRequestException::throwException(12003, 'Missing /verified/ in election config'     , "request received: \n" . print_r($req, true));
 		if ( (! isset($req["eligible"]     )) || (! is_bool(  $req['eligible']     )) ) WrongRequestException::throwException(12004, 'Missing /eligible/ in election config'     , "request received: \n" . print_r($req, true));
-		if ( (! isset($electionId          )) || (! is_string($electionId          )) ) WrongRequestException::throwException(12005, '/ElectionId/ not set or of wrong type', "request received: \n" . print_r($req, true));
+		if ( (! isset($electionId          )) || (! is_string($electionId          )) ) WrongRequestException::throwException(12005, '/ElectionId/ not set or of wrong type'     , "request received: \n" . print_r($req, true));
+		if ( ! (in_array($req['serverId'], $oauthConfig))  )                            WrongRequestException::throwException(12007, 'Configuration for OAuth2-serverId not found' , "request received: \n" . print_r($req, true));
 		$authconfig['serverId']      = $req['serverId'];
 		$authconfig['listId']        = $req["listId"];
 		$authconfig['nested_groups'] = $req["nested_groups"];

backend/config/conf-thisserver-example.php
@@ -28,7 +28,8 @@ $dbInfos = array(
 require_once __DIR__ . '/../rsaMyExts.php';
 
 $serverNo = 1;
-if (isset($_SERVER['HTTP_HOST']) && $_SERVER['HTTP_HOST'] !== parse_url($pServerUrlBases[$serverNo -1],  PHP_URL_HOST)) {
+if (isset($_SERVER['HTTP_HOST'])   && $_SERVER['HTTP_HOST']   !== parse_url($pServerUrlBases[$serverNo -1],  PHP_URL_HOST) 
+ && isset($_SERVER["SERVER_PORT"]) && $_SERVER['SERVER_PORT'] !== parse_url($pServerUrlBases[$serverNo -1],  PHP_URL_PORT) ) {
 	require_once 'conf-thisserver2.php';
 } else {
 	date_default_timezone_set('Europe/Berlin'); // this is only used to avoid a warning message from PHP -> you do not need to adjust it. All dates are in UTC or use explicit time zone offset.

webclient/modules-auth/oauth2/module.js
@@ -104,7 +104,9 @@ OAuth2.getMainContent = function(conf) {
 	// 'Ihre Wahlberechtigung abzurufen.' +
 		'<br>';
 //	step++;
-	
+	if (! ClientConfig.oAuth2Config[serverId]) {
+		alert(i18n.sprintf(i18n.gettext("Configuration error: serverId >%s< is asked for, but not configured"), serverId)); 
+	}
 	var slist = ClientConfig.serverList;
 	for ( var permissionServerId in ClientConfig.oAuth2Config[serverId].clientId) {
 		var clientId = ClientConfig.oAuth2Config[conf.authConfig.serverId].clientId[permissionServerId];
@@ -168,7 +170,7 @@ OAuth2.getNewElectionData = function (serverId) {
 			"RegistrationStartDate": "2014-01-27T21:20:00Z",
 			"RegistrationEndDate":   "2020-10-10T21:20:00Z"
 			};
-	ret.authData.serverId = ClientConfig.oAuth2Config[serverId].serverId; // TODO read this from selected OAuthServer-config which was selected in the web formular 
+	ret.authData.serverId = ClientConfig.oAuth2Config[serverId].serverId; 
 	var element = document.getElementById('listId');
 	ret.authData.listId = element.value;
 	return ret;

commit 192654d542e9b122b4b4124cafba2ae94c3ab9e9

backend: bug fixed which was introduced with commit 74afa51bac0b94bfdf6fbf99d95d42029df0a5a9 which should return a better error message

backend/modules-auth/oauth/auth.php
@@ -179,7 +179,8 @@ class OAuth2 extends Auth {
 		if ( (! isset($req["verified"]     )) || (! is_bool(  $req['verified']     )) ) WrongRequestException::throwException(12003, 'Missing /verified/ in election config'     , "request received: \n" . print_r($req, true));
 		if ( (! isset($req["eligible"]     )) || (! is_bool(  $req['eligible']     )) ) WrongRequestException::throwException(12004, 'Missing /eligible/ in election config'     , "request received: \n" . print_r($req, true));
 		if ( (! isset($electionId          )) || (! is_string($electionId          )) ) WrongRequestException::throwException(12005, '/ElectionId/ not set or of wrong type'     , "request received: \n" . print_r($req, true));
-		if ( ! (in_array($req['serverId'], $oauthConfig))  )                            WrongRequestException::throwException(12007, 'Configuration for OAuth2-serverId not found' , "request received: \n" . print_r($req, true));
+        global $oauthConfig;
+  		if ( ! (array_key_exists($req['serverId'], $oauthConfig))  )                            WrongRequestException::throwException(12007, 'Configuration for OAuth2-serverId not found' , "request received: \n" . print_r($req, true));
 		$authconfig['serverId']      = $req['serverId'];
 		$authconfig['listId']        = $req["listId"];
 		$authconfig['nested_groups'] = $req["nested_groups"];