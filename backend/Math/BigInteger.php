adb355889366d5528bba8c21fdaa60a7decb5fc1
pfefffer <vvvote@robert-arnold.de>
2020-03-28 13:40:33 +0100

    BigInteger: re-add security 404 when called directly

--- backend/Math/BigInteger.php
+++ backend/Math/BigInteger.php
@@ -4,6 +4,12 @@
  * return 404 if called directly
  * added by Pfefffer
  */
+if(count(get_included_files()) < 2) {
+    header('HTTP/1.0 404 Not Found');
+    echo "<h1>404 Not Found</h1>";
+    echo "The page that you have requested could not be found.";
+    exit;
+}
 
 /**
  * Source: https://github.com/phpseclib/phpseclib/blob/1.0/phpseclib/Math/BigInteger.php
