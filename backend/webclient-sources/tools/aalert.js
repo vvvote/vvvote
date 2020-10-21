d6b67f8507941e04b969123c1fe53c8c7df39ac0
pfefffer <abclkmflkmremf@deljfjfhbfevf.gh>
2020-10-21 10:37:48 +0200

    add file that was missing in the previous commit

---
+++ backend/webclient-sources/tools/aalert.js
@@ -0,0 +1,25 @@
+/**
+ * This replaces the alert() function which has restrictions on some browsers
+ */
+
+var aalert = {
+  openHtml: function (title, message) {
+		var htmlStr = message +
+		'<p><button id="okbuttonid" type="button" autofocus="autofocus" onclick="removePopup();">' + 
+		i18n.gettext('Ok') +
+		'</button></p>';
+		var fragm = html2Fragm(htmlStr);
+		showPopup(fragm);
+		var el = document.getElementById('okbuttonid');
+		el.focus();
+  },
+  openTextOk: function (message) {
+		var htmlStr = '<div id="popuptext"></div>';
+		this.openHtml("", htmlStr);
+		var el = document.getElementById('popuptext');
+		el.innerText = message;
+	  },
+  close: function () {
+    document.getElementById("dialog-wrap").style.display = "none";
+  }
+};
